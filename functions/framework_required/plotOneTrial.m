8425e8c7ee169882fed6ce268fafedc90bd9ecc0
MeijerDavid <58221670+MeijerDavid@users.noreply.github.com>
2024-06-04 21:33:44 +0200

    Small consistency update of argument names

--- functions/framework_required/plotOneTrial.m
+++ functions/framework_required/plotOneTrial.m
@@ -1,4 +1,4 @@
-function plotOneTrial(trial_struct,response_struct,prediction_struct,cond_nr,param_settings,model_settings,fit_settings)
+function plotOneTrial(trial_data,trial_responses,pred_trial_struct,cond_nr,param_settings,model_settings,fit_settings)
 %Plot predictions for one trial
     
 end %[EoF]
